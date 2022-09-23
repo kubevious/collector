6f399fb9674c26b60c52aaa875d8dddb78999d01
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-09-23 15:16:23 -0700

    Added UBI image build.

--- configuration.sh
+++ configuration.sh
@@ -2,6 +2,7 @@ export SERVER_PORT=4003
 export CONTAINER_NAME=kubevious-collector
 export NETWORK_NAME=kubevious
 export IMAGE_NAME=kubevious-collector
+export IMAGE_NAME_UBI=${IMAGE_NAME}-ubi
 
 export BACKEND_BASE_URL=http://localhost:4002
 
